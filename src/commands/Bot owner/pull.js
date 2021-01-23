01285d56d84a6cda6752ad6fff6c35115b995263
Trywin <67023288+trywin0@users.noreply.github.com>
2021-01-23 03:01:28 +0100

    Update pull.js

--- src/commands/Bot owner/pull.js
+++ src/commands/Bot owner/pull.js
@@ -3,7 +3,7 @@ const path = __dirname + '/../'
 
 function pull(){
   shell.cd(path)
-  shell.exec('git clone https://github.com/trywin0/Astral-Bot')
+  shell.exec('git pull https://github.com/trywin0/Astral-Bot')
 }
 module.exports = {
   name: 'pull',
